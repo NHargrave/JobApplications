49c0ba11bdb19320d63e42a23002e69986bd2120
NHargrave <nathaniel.hargrave@tx.rr.com>
2022-06-19 18:05:03 -0500

    Finished implementing RTSLabs exercise

--- src/RTSLabsApplication.java
+++ src/RTSLabsApplication.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 /*
 RTS Labs Senior Back-end Developer Position
@@ -27,16 +29,52 @@ Please write a class in the language of your choice that contains the following 
 
 class RTSLabsApplication {
 	public static void main(String args[]) {
+		RTSLabsApplication tester = new RTSLabsApplication();
 		
+		// Test aboveBelow
+		List<Integer> intList = Arrays.asList(1, 5, 2, 6, 12, 34, 11, 100);
+		int comparisonValue = 3;
+
+		System.out.println("Testing aboveBelow");
+		System.out.println("------------------");
+		System.out.println("input: " + intList + ", " + comparisonValue);
+		System.out.println("output: " + tester.aboveBelow(intList, comparisonValue));
+
+		// Test stringRotation
+		String input = "MyString";
+		int rotation = 9;
+		
+		System.out.println("\nTesting stringRotation");
+		System.out.println("----------------------");
+		System.out.println("input: " + input + ", " + rotation);
+		System.out.println("output: " + tester.stringRotation(input, rotation));
 	}
-	
+
+	// return counts of numbers in a list above/below a given number
+	// what to do with equal numbers?
 	public HashMap<String, Integer> aboveBelow(Collection<Integer> intList, int theBar) {
 		HashMap<String, Integer> aboveBelowCounts = new HashMap<String, Integer>();
+		int above = 0, below = 0;
+
+		for (int num : intList) {
+			if (num > theBar)
+				above++;
+			else if (num < theBar)
+				below++;
+		}
+
+		aboveBelowCounts.put("above", above);
+		aboveBelowCounts.put("below", below);
+
 		return aboveBelowCounts;
 	}
-	
+
 	public String stringRotation(String input, int rotator) {
-		String rotated = "";
-		return rotated;
+		// Protect against rotations larger than input String
+		rotator = rotator % input.length();
+		// Cheating a little - turning this into equivalent left rotate
+		rotator = input.length() - rotator;
+		
+		return input.substring(rotator) + input.substring(0, rotator);
 	}
 }
